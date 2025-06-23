092f0d6ba41c705d08d58f79d4c3710f776d8ace
Kévin Walcarius <kevin.walcarius@cnes.fr>
2025-06-23 18:02:00 +0200

    Adding base grid and pfgrid from Smart-G to constants.

--- general/constants.py
+++ general/constants.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-import os
+import numpy as np
 
 ROOT_PATH = Path(__file__).resolve().parent.parent
 IMG_PATH = ROOT_PATH / "img"
+
+GRID: np.ndarray = np.linspace(100., 0., num=101)
+PFGRID: np.ndarray = np.array([100., 20., 10., 9., 8., 7., 6., 5., 4., 3., 2., 1., 0.])
