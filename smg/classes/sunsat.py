d888868a7f2da40eb2e7533b1515d5f981e808c5
Kévin Walcarius <kevin.walcarius@cnes.fr>
2025-06-23 14:53:18 +0200

    Base class SunSat to encapsulate the sun / sensor geometry (sza, saa, vza, vaa, sat_height).

---
+++ smg/classes/sunsat.py
@@ -0,0 +1,56 @@
+from smartg.smartg import Sensor
+from dataclasses import dataclass
+from typing import Dict, Tuple
+import numpy as np
+
+@dataclass 
+class SunSat: 
+    sza_deg: float = 0.0
+    saa_deg: float = 0.0
+    vza_deg: float = 0.0
+    vaa_deg: float = 0.0
+    sat_height: float = 700.0
+
+    @property
+    def sun_le(self) -> Dict[str, float]:
+        return self._le(self.sza_deg, self.saa_deg)
+    
+    @property
+    def sat_le(self) -> Dict[str, float]:
+        return self._le(self.vza_deg, self.vaa_deg)
+    
+    @property
+    def sun_sensor(self) -> Sensor:
+        return self._sensor(self.sat_height, self.sza_deg, self.saa_deg)
+
+    @property
+    def sat_sensor(self) -> Sensor:
+        return self._sensor(self.sat_height, self.vza_deg, self.vaa_deg)
+    
+    @property
+    def satellite_relative_position(self) -> Tuple[float, float]:
+        """
+        Returns the relative position (x, y) of a satellite from 
+        the point P(x=0, y=0, z=0) it is looking at on the earth.
+        """
+        tan_vza: float = np.tan(np.radians(self.vza_deg))
+        cos_vaa: float = np.cos(np.radians(180 - self.vaa_deg))
+        sin_vaa: float = np.sin(np.radians(180 - self.vaa_deg))
+        x: float  = (self.sat_height * tan_vza) * cos_vaa
+        y: float = (self.sat_height * tan_vza) * sin_vaa
+        print("Computed satellite relative position: (%.2f, %.2f)", x, y)
+        return (np.round(x, 4), np.round(y, 4))
+
+    def _le(self, za: float, aa: float) -> Dict[str, float]:
+        return {"th_deg": za, "phi_deg": aa, "zip": True}
+
+    def _sensor(self, height: float, za: float, aa: float) -> Sensor:
+        return Sensor(POSZ=height, THDEG=180.0 - za, PHDEG=aa, LOC='ATMOS')                   
+
+    
+    def __str__(self) -> str:
+        return "-".join([
+            f"SZA-{self.sza_deg}", f"SAA-{self.saa_deg}",
+            f"VZA-{self.vza_deg}", f"VAA-{self.vaa_deg}",
+            f"H-{self.sat_height}"
+        ])
